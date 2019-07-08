ccb491e956cc6c6d3715acce710f3d0874f0fa54
Emma Casper <45472594+emmacasper@users.noreply.github.com>
2019-07-07 20:15:29 -0400

    part 2 and 3 of set1 (xor) work, probably want to mess with oov character parameters later

--- cryptopals/set1.py
+++ cryptopals/set1.py
@@ -1,15 +1,46 @@
 
 from set1utils import hex2b64
+from set1utils import hexxor
+from set1utils import hex2bytes, make_punctuation_normalizer, guess_single_key, eng_lfreq
 
 ### part 1 ###
-part1mesg = b'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+q1mesg = b'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 
-part1ans = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+q1ans = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
 def run_part1():
-    print(hex2b64(part1mesg))
+    print('running part 1...')
+    print(hex2b64(q1mesg))
     print('should be')
-    print(part1ans)
+    print(q1ans)
 
 
+### part 2 ###
+
+q2m1 = b'1c0111001f010100061a024b53535009181c'
+q2m2 = b'686974207468652062756c6c277320657965'
+q2ans = b'746865206b696420646f6e277420706c6179'
+
+def run_part2():
+    print('running part 2...')
+    print(hexxor(q2m1, q2m2))
+    print('should b')
+    print(q2ans)
+
+### part 3 ###
+
+q3m = b'1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+
+def run_part3():
+    print('running part 3...')
+    oov = make_punctuation_normalizer(eng_lfreq)
+    k, ans, ll = guess_single_key(hex2bytes(q3m), eng_lfreq, oov)
+    print('message is:', bytes(ans))
+    print('key was:', k)
+    print('log likelihood is around:', ll)
+
+if __name__ == '__main__':
+    #run_part1()
+    #run_part2()
+    run_part3()
 
--- cryptopals/set1utils.py
+++ cryptopals/set1utils.py
@@ -1,4 +1,5 @@
 
+import math
 
 def hex2bytes(x):
     """turns a hex string (string or bytes) into the equaivalent values in bytes"""
@@ -54,3 +55,67 @@ def hex2b64(x):
         print(b''.join(result))
         result.append(chunk64last(x[-(len(x)%3):]))
     return b''.join(result)
+
+###############################
+
+def bytexor(s1, s2):
+    return [c1^c2 for (c1, c2) in zip(s1, s2)]
+
+def hexxor(h1, h2):
+    return b''.join([int2base(x, alph_h, 2) for x in bytexor(hex2bytes(h1), hex2bytes(h2))])
+
+
+eng_freq = {'a': 8.167, 'b': 1.492, 'c': 2.782, 'd': 4.253, 'e': 12.702,
+        'f': 2.228, 'g': 2.015, 'h': 6.094, 'i': 6.966, 'j': 0.153, 'k': 0.772,
+        'l': 4.025, 'm': 2.406, 'n': 6.749, 'o': 7.507, 'p': 1.929, 'q': 0.095,
+        'r': 5.987, 's': 6.327, 't': 2.758, 'u': 2.758, 'v': 0.987, 'w': 2.360,
+        'x': 0.150, 'y': 1.974, 'z': 0.074}
+
+eng_lfreq = {ord(k): math.log(v/100) for (k, v) in eng_freq.items()}
+
+def likelihood(s, alph, oov):
+    """Returns the log likelihood of the text s given the
+    character distributions alph (log probabilities).
+    oov is a function which takes a character which is used
+    to handle characters not in alph
+    """
+    s = [alph[c] if c in alph.keys() else oov(c) for c in s]
+    #print(s)
+    return sum(s)
+
+def make_punctuation_normalizer(alph):
+    """ignores case, (updates alph to ignore case by adding items)
+    assigns ok probability to spaces,
+    somewhat low probability to common punctuation,
+    and very low probability to everything else.
+    *** the distribution no longer sums to 1 though
+
+    *** need to decide on exact values for oov
+    I would think we like common punctuation more than this
+    but 8*m is not enough to keep from getting gibberish
+    ** fine tune this on later problems?
+    """
+    m = min(alph.values())
+    alph.update({k-32: v for (k, v) in alph.items()})
+    def oov(c):
+        if c == b' ':
+            return 0
+        if c in b',.?!()&-+=\'":;':
+            return 9*m
+        else:
+            return 10*m
+    print('updated keys to include uppercase:', len(alph.keys()))
+    return oov
+
+def guess_single_key(s, alph, oov, start=0, end=128):
+    """Returns the max likelihood estimate of key xor'ed with s
+    using alph as character likelihoods
+    and oov as oov function.
+    checks values between start and end (default 0, 128)
+    """
+    ms = [bytexor(s, [k]*len(s)) for k in range(start, end)]
+    ls = [likelihood(m, alph, oov) for m in ms]
+    besti = max(range(start, end), key=lambda x: ls[x-start])
+    return besti, ms[besti], ls[besti]
+
+
